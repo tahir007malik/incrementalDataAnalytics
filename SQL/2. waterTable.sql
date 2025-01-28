6f01d52cc3b30a3c91745e7d912cc218ef7fcab3
tahir007malik <tahir.malik296@gmail.com>
2025-01-28 17:27:51 +0000

    added comments to watertable.sql

--- SQL/2. waterTable.sql
+++ SQL/2. waterTable.sql
@@ -2,4 +2,6 @@ CREATE TABLE water_table (
     last_load VARCHAR(2000),
 )
 
+-- Inserting lowest value for `date_id` to `water_table`
+-- This will let us take all records for the initial data load
 INSERT INTO water_table VALUES('DT00000');
